20389eb65d2fbc3feb2f86425fb7a5274ddcece9
ya0guang <ya0guang@protonmail.com>
2022-03-22 17:26:40 -0700

    add violation features

--- enclave/README.md
+++ enclave/README.md
@@ -6,6 +6,18 @@ The main part of PoBF is verified towards the Rust compiler and MIRAI. The glue 
 
 Since we only need to verify the implementation of PoBF in enclaves, successfully compilation **and** MIRAI executed without complians implies that the enclave is verified to hold PoBF properties.
 
+
 ### Usage
 
 `make` will compile the enclave using Rust compiler, such that security checkes (e.g., type, memory safety) are enforced; `make mirai` runs MIRAI on the project, where potential leakage can be detected with warnnings.
+
+### Violations
+
+Here is a list of potential violations:
+
+- Typestate violation: `vio_typestate`
+- Unexpected unsafe code: `vio_unsafe`
+- Structure privacy violation: `vio_privacy`
+
+If you want to try to compile code that violates the PoBF rules, you can run`cargo build --features "vio_X"`, where all site that violates the corresponding rule can found by the Rust compiler.
+Moreover, if you want to check all the violations, simply run `cargo build --features "violation"`.
--- enclave/src/pobf.rs
+++ enclave/src/pobf.rs
@@ -13,6 +13,17 @@ pub fn pobf_sample_task_aaes(
     let input_key: AES128Key = AES128Key::from_sealed_buffer(sealed_key_buffer)?;
     let output_key: AES128Key = AES128Key::from_sealed_buffer(sealed_key_buffer)?;
     println!("PoBF sample task AES started...");
+
+    // privacy violation: cannot see through the key
+    // captured by: compiler error
+    #[cfg(feature = "vio_privacy")]
+    let raw_key = input_key.inner;
+
+    // safety violation: cannot see through the key using unsafe code by derefencing it
+    // captured by: compiler error
+    #[cfg(feature = "vio_unsafe")]
+    let raw_key = unsafe { *(&input_key as *const AES128Key as *const u8) };
+
     // custom compuatation task
     let computation_task = &computation_aaes;
 
@@ -51,28 +62,38 @@ pub fn computation_vaes(data: VecAESData) -> VecAESData {
     VecAESData::new(new)
 }
 
-pub fn pobf_ref_implementation<T: EncDec<K>, K: Default>(
-    input_sealed: T,
+pub fn pobf_ref_implementation<D: EncDec<K>, K: Default>(
+    input_sealed: D,
     input_key: K,
     output_key: K,
-    computation_task: &dyn Fn(T) -> T,
-) -> SgxResult<T> {
-    let protected_enc_in = ProtectedAssets::new(input_sealed, input_key, output_key);
+    computation_task: &dyn Fn(D) -> D,
+) -> SgxResult<D> {
+    let en_in: ProtectedAssets<Encrypted, Input, D, K> =
+        ProtectedAssets::new(input_sealed, input_key, output_key);
 
-    let protected_dec_in = protected_enc_in.decrypt()?;
+    let de_in: ProtectedAssets<Decrypted, Input, D, K> = en_in.decrypt()?;
 
-    let protected_dec_out = protected_dec_in.invoke(computation_task)?;
+    // unsafe code violation: cannot use (unsafe) raw pointer dereferrence to steal data
 
-    let protected_enc_out = protected_dec_out.encrypt()?;
+    // typestate violation: cannot take inner data from decrypted data
+    // captured by: compiler error
+    #[cfg(feature = "vio_typestate")]
+    let de_in_data = de_in.take();
 
-    Ok(protected_enc_out.take())
+    let de_out: ProtectedAssets<Decrypted, Output, D, K> = de_in.invoke(computation_task)?;
+
+    // privacy violation: cannot take the inner data from ProtectedAssets
+    // captured by: compiler error
+    #[cfg(feature = "vio_privacy")]
+    let de_out_data = de_out.data;
+
+    let en_out: ProtectedAssets<Encrypted, Output, D, K> = de_out.encrypt()?;
+
+    Ok(en_out.take())
 }
 
 // data to_vec!
 pub fn computation_aaes(data: ArrayAESData) -> ArrayAESData {
-    // try to violate key? Cannot see the key!
-    // conditional compile some errors
-
     let mut output_data = ArrayAESData::new([0u8; BUFFER_SIZE], data.mac, data.length);
 
     let step = 1;
@@ -84,6 +105,8 @@ pub fn computation_aaes(data: ArrayAESData) -> ArrayAESData {
     }
 
     // however, MIRAI complians about this
+    // leakage violation: cannot log the secret data
+    // captured by: MIRAI warnning
     ocall_log!(
         "after increasing, the 0th data is {}",
         1,
@@ -106,4 +129,3 @@ pub fn computation_sealed(data: SealedData) -> SealedData {
     }
     new_data
 }
-
