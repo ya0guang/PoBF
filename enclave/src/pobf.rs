7018b6ad4ea81bb1e83ca306fa252cb940a2e3cb
Haobin Chen <etyyuiope@qq.com>
2022-08-12 22:52:12 -0400

    Added additional stack / data zeroization.

--- enclave/src/pobf.rs
+++ enclave/src/pobf.rs
@@ -5,7 +5,9 @@ use crate::types::*;
 use crate::userfunc::vec_inc;
 use crate::{ocall_log, verified_log};
 use alloc::vec::Vec;
+use clear_on_drop::clear_stack_on_return_fnonce;
 use sgx_types::error::SgxResult;
+use zeroize::Zeroize;
 
 pub fn pobf_private_computing(
     data_buffer: &[u8],
@@ -13,7 +15,7 @@ pub fn pobf_private_computing(
 ) -> SgxResult<VecAESData> {
     verified_log!("PoBF sample task AES started...");
     // initialize data from buffer
-    let input_key = AES128Key::from_sealed_buffer(sealed_key_buffer)?;
+    let input_key =  AES128Key::from_sealed_buffer(sealed_key_buffer)?;
     let output_key = AES128Key::from_sealed_buffer(sealed_key_buffer)?;
     let data = VecAESData::from(data_buffer);
 
@@ -39,11 +41,11 @@ pub fn pobf_private_computing(
         *(0x3ffffff as *const u8 as *mut u8) = data_buffer[1];
     }
 
-    // custom compuatation task
+    // custom computation task
     let computation_task = &private_vec_compute;
-    let result = pobf_workflow(data, input_key, output_key, computation_task);
+    let f = || pobf_workflow(data, input_key, output_key, computation_task);
 
-    result
+    clear_stack_on_return_fnonce(crate::DEFAULT_PAGE_SIZE_LEAF, f, true)
 }
 
 pub fn private_vec_compute<T>(input: T) -> T
@@ -61,13 +63,16 @@ pub fn pobf_workflow<D: EncDec<K>, K: Default>(
     input_key: K,
     output_key: K,
     computation_task: &dyn Fn(D) -> D,
-) -> SgxResult<D> {
+) -> SgxResult<D>
+    where
+        K: Zeroize
+{
     let enc_in: ProtectedAssets<Encrypted, Input, D, K> =
         ProtectedAssets::new(input_sealed, input_key, output_key);
 
     let dec_in: ProtectedAssets<Decrypted, Input, D, K> = enc_in.decrypt()?;
 
-    // typestate violation: cannot take inner data from decrypted data
+    // Typestate violation: cannot take inner data from decrypted data
     // captured by: compiler error
     #[cfg(feature = "disallowed_trans")]
     let dec_in_data = dec_in.take();
@@ -83,5 +88,6 @@ pub fn pobf_workflow<D: EncDec<K>, K: Default>(
     let de_out_data = dec_out.data;
 
     let en_out: ProtectedAssets<Encrypted, Output, D, K> = dec_out.encrypt()?;
+
     Ok(en_out.take())
 }
