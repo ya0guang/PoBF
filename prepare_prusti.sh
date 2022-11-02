b77abf383762a6710bcbc9fd6fadddd14ce664c0
Hiroki Chen <etyyuiope@qq.com>
2022-11-02 09:15:11 -0400

    Update toolchain to `nightly-11-01`

--- prepare_prusti.sh
+++ prepare_prusti.sh
@@ -15,9 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-commit="734426eb50e6de7454b40694d5a8e843c05d9ee8"
+commit="ee91e7772e764c910b1e6638f609ad5da0a791a7"
 rust_comp="rust-src rustc-dev llvm-tools-preview rustfmt rust-analysis"
-rust_toolchain="nightly-2022-10-22"
+rust_toolchain=$(cat ./rust-toolchain)
 
 # Clone this repo.
 git clone https://github.com/viperproject/prusti-dev.git $HOME/prusti-dev
