63cf61ab98d628b93c51db7d9dd3d4e2d582096c
Hiroki Chen <etyyuiope@qq.com>
2023-05-16 23:39:25 -0400

    Update rust application

--- others/rust_app/client/src/main.rs
+++ others/rust_app/client/src/main.rs
@@ -3,6 +3,9 @@ use std::fs;
 use std::io::*;
 use std::net::TcpStream;
 
+use pobf_crypto::handle_sev_pubkey;
+use pobf_crypto::init_keypair;
+
 const ADDRESS: &str = "127.0.0.1:7788";
 
 fn main() {
@@ -16,21 +19,36 @@ fn main() {
     let data = fs::read(data_path).unwrap();
     println!("Reading {}", data_path);
 
-    // Send to the server.
-    writer.write(data.len().to_string().as_bytes()).unwrap();
-    writer.write(b"\n").unwrap();
-    writer.flush().unwrap();
-    writer.write(&data).unwrap();
-    writer.write(b"\n").unwrap();
-    writer.flush().unwrap();
-    println!("Sent data. Length = {}", data.len());
+    let mut key_pair = init_keypair().unwrap();
+    let public_key = &key_pair.pub_k;
 
-    let mut length_str = String::with_capacity(512);
-    reader.read_line(&mut length_str).unwrap();
-    let data_len = length_str[..length_str.len() - 1].parse::<usize>().unwrap();
-    println!("Data length = {}", data_len);
-    let mut input = vec![0u8; data_len];
-    reader.read_exact(&mut input).unwrap();
-    println!("Read data.");
-    println!("Finished");
+    println!("[+] Sending public key");
+    writer.write_all(public_key.as_ref()).unwrap();
+    writer.flush().unwrap();
+
+    println!("[+] Receiving peer public key");
+    let peer_pub_key = handle_sev_pubkey(&mut reader).unwrap();
+    key_pair.compute_shared_key(&peer_pub_key, b"").unwrap();
+
+    println!("[+] Sending data...");
+    // Read the data and encrypt it.
+    let data = key_pair.encrypt_with_smk(&data).unwrap();
+    writer.write_all(data.len().to_string().as_bytes()).unwrap();
+    writer.write_all(b"\n").unwrap();
+    writer.flush().unwrap();
+    writer.write_all(&data).unwrap();
+    writer.flush().unwrap();
+
+    println!("[+] Receiving the data...");
+
+    let mut len = String::with_capacity(128);
+    reader.read_line(&mut len).unwrap();
+    let buf_len = len[..len.len() - 1].parse::<usize>().unwrap();
+    let mut buf = vec![0u8; buf_len];
+    reader.read_exact(&mut buf).unwrap();
+
+    // Decrypt the data.
+    let out = key_pair.decrypt_with_smk(&buf).unwrap();
+
+    std::fs::write("./data.txt", out).unwrap();
 }
